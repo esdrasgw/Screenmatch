910c3c420271e1ebbf82a750fd579024973b1ff6
Esdras <esdrasgw@gmail.com>
2025-06-05 09:11:41 -0300

    inicio do projeto Spring

---
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,27 @@
+package br.com.alura.screenmatch;
+
+import br.com.alura.screenmatch.model.DadosSerie;
+import br.com.alura.screenmatch.services.ConsumoAPI;
+import br.com.alura.screenmatch.services.ConverteDados;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var consumoAPI = new ConsumoAPI();
+		var json = consumoAPI.obterDados("https://www.omdbapi.com/?i=tt1475582&apikey=6bcee7e7");
+		System.out.println(json);
+
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
+	}
+}
